4b0fd4e48722502829d8c3603848dca87ceee871
ashraf-rahil-shaik <skashraf97@gmail.com>
2024-03-13 15:24:04 +0530

    added movies api and added data to redux store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { options } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovie } from '../utils/movieSlice';
 
 const Browse = () => {
+  const dispatch =useDispatch()
+
+const getNowPlaying = async() =>{
+const data = await fetch(
+  'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',options)
+const json = await data.json();
+console.log(json.results)
+dispatch(addNowPlayingMovie(json.results))
+}
+
+useEffect(()=>{
+getNowPlaying()
+},[])
+
+
+
+
   return (
     <div>
       
---
+++ src/utils/constants.jsx
@@ -0,0 +1,9 @@
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmQxODc1OTNjZThlZjkzMWNiNDgyM2MzOTEzNjc2YyIsInN1YiI6IjYyN2JjMDc3NzEzZWE2MDA2OTU3NTgzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AnsoFL3k5ZIMI9smyCWeBwA1qVY6yEvg2OTVCyKymtc'
+    }
+  };
+
+  
